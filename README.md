de33dd4fe354edde73b4e4a29765137cc31ef9f2
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-10-17 19:26:40 +0900

    fierst commit

--- README.md
+++ README.md
@@ -1,1 +1,54 @@
 # amazonapi
+
+
+###******* WARNING ******
+Rewrite the following code.
+You need to comment out.
+
+
+github.com/svvu/gomws/gmws/base.go#NewMwsBase()
+```github.com/svvu/gomws/gmws/base.go
+	if config.SellerId == "" {
+		return nil, fmt.Errorf("No seller id provided")
+	}
+	// ********** This code. *********
+	//if config.AuthToken == "" {
+	//	return nil, fmt.Errorf("No auth token provided")
+	//}
+
+	region := config.Region
+	if region == "" {
+		region = "US"
+	}
+
+	marketPlace, mError := marketplace.New(region)
+	if mError != nil {
+		return nil, mError
+	}
+
+	base := MwsBase{
+		SellerId:      config.SellerId,
+		AuthToken:     config.AuthToken,
+		Region:        region,
+		MarketPlaceId: marketPlace.Id,
+		Host:          marketPlace.EndPoint,
+		Version:       version,
+		Name:          name,
+		accessKey:     config.AccessKey,
+		secretKey:     config.SecretKey,
+	}
+	return &base, nil
+```
+github.com/svvu/gomws/gmws/base.go#paramsToAugment()
+```github.com/svvu/gomws/gmws/base.go
+	clientInfo := map[string]string{
+		"SellerId":         base.SellerId,
+		// ********** This code. *********
+		//"MWSAuthToken":     base.AuthToken,
+		"SignatureMethod":  base.SignatureMethod(),
+		"SignatureVersion": base.SignatureVersion(),
+		"AWSAccessKeyId":   base.getCredential().AccessKey,
+		"Version":          base.Version,
+	}
+	return clientInfo
+```
---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"github.com/svvu/gomws/gmws"
+	"github.com/svvu/gomws/mws/products"
+	"log"
+	"os"
+	"fmt"
+	"net/http"
+)
+
+
+func main() {
+	fmt.Println()
+	// API key config
+	config := gmws.MwsConfig{
+		SellerId:  os.Getenv("SellerId"),
+		AccessKey: os.Getenv("AccessKey"),
+		SecretKey: os.Getenv("SecretKey"),
+		Region:    "JP",
+	}
+
+	// Create client
+	productsClient, err := products.NewClient(config)
+	if err != nil{
+		log.Println(err)
+		return
+	}
+
+	// Send request
+	response := productsClient.GetLowestOfferListingsForASIN([]string{"B00JPKHFTA","B075VQ6RFZ"})
+	if response.Error != nil || response.StatusCode != http.StatusOK {
+		log.Println("http Status:" + string(response.StatusCode))
+		log.Println(response.Error)
+		return
+	}
+
+	// responseXML to XMLNode
+	xmlNode, err := gmws.GenerateXMLNode(resaponse.Body)
+	if gmws.HasErrors(xmlNode) {
+		log.Println(gmws.GetErrors(xmlNode))
+		return
+	}
+
+	// XML Parse
+	xmlNode.PrintXML()
+
+	// Get all products
+	products := xmlNode.FindByKey("GetLowestOfferListingsForASINResult")
+	// products to one product
+	for _, product := range products{
+		// ASIN
+		fmt.Println(product.FindByPath("Product.Identifiers.MarketplaceASIN.ASIN")[0].Value)
+		// Get all prices
+		prices := product.FindByPath("Product.LowestOfferListings.LowestOfferListing.Price.LandedPrice.Amount")
+		// prices to one price
+		for _, price := range prices{
+			fmt.Println(price.Value)
+		}
+	}
+}
+
